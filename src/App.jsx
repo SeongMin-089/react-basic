fe211b239a6fc1bde121133b6322fadab04e83b3
unknown <a99701841@gmail.com>
2026-02-02 11:35:24 +0900

    react basic

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Aisde from './components/Aisde'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Header/>
+      <Aisde/>
+      <h2>hello react!</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Aisde.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aisde = () => {
+  return (
+    <aside>Aisde</aside>
+  )
+}
+
+export default Aisde
