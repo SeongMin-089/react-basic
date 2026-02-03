4a019bfc8fd792e5152b4f6b32473834a2cb5984
unknown <a99701841@gmail.com>
2026-02-03 10:03:01 +0900

    delete

--- src/components/Article.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Article() {
-  return (
-    <article>Article</article>
-  )
-}
-
-export default Article
--- src/components/Aside.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Aside() {
-  return (
-    <aside>Aside</aside>
-  )
-}
-
-export default Aside
