d38ca974c92b6ce7e8ddb33043cf36b2e91d8e9e
unknown <a99701841@gmail.com>
2026-02-13 09:39:10 +0900

    update

---
+++ src/context/MyProvider.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react'
+import { MyContext } from './MyContact'
+export const MyProvider = ({children}) => {
+    const [value, setValue]=useState("Hello Context!")
+  return (
+    <MyContext.Provider value={{value,setValue}}>
+        {children}
+    </MyContext.Provider>
+  )
+}
